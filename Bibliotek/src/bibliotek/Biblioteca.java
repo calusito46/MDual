c825c49f630886893eea5f5287491af2f40e23ba
Eduard Pujol <calusito46@gmail.com>
2018-10-22 15:59:43 +0200

    function getNMaxLlibres i getNActualLlibres fetes

--- Bibliotek/src/bibliotek/Biblioteca.java
+++ Bibliotek/src/bibliotek/Biblioteca.java
@@ -10,5 +10,27 @@ package bibliotek;
  * @author Calusito
  */
 public class Biblioteca {
+
+    private Llibre[] llibres;
+    private int nLlibres;
+
+    public Biblioteca(int maxLlibres) {
+        this.llibres = new Llibre[maxLlibres];
+    }
     
+    public int getNMaxLlibres(){
+        return llibres.length;        
+    }
+    public int getNActualLlibres() {
+        int cont = 0;
+        for (Llibre llibre : llibres) {
+            if (llibre != null) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+    public void afegirLlibre() {
+        
+    }
 }
--- Bibliotek/src/bibliotek/Llibre.java
+++ Bibliotek/src/bibliotek/Llibre.java
@@ -14,12 +14,13 @@ public class Llibre {
     private String isbn;
     private String author;
     private Boolean cedit;
-      public Llibre(String titol, String isbn, String autor) {
-          this.title = titol;
-          this.isbn = isbn;
-          this.author = autor;
-          this.cedit = false;
-      }
+
+    public Llibre(String titol, String isbn, String autor) {
+        this.title = titol;
+        this.isbn = isbn;
+        this.author = autor;
+        this.cedit = false;
+    }
       public void imprimir(){
           System.out.println(this.title);
           System.out.println(this.isbn);
