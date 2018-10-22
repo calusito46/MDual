74761efa3bf8bb524159fdeb4bc65ae1a97abada
Eduard Pujol <calusito46@gmail.com>
2018-10-22 15:14:55 +0200

    function mateix autor added

--- Bibliotek/src/bibliotek/Llibre.java
+++ Bibliotek/src/bibliotek/Llibre.java
@@ -13,10 +13,12 @@ public class Llibre {
     private String title;
     private String isbn;
     private String author;
+    private Boolean cedit;
       public Llibre(String titol, String isbn, String autor) {
           this.title = titol;
           this.isbn = isbn;
           this.author = autor;
+          this.cedit = false;
       }
       public void imprimir(){
           System.out.println(this.title);
@@ -24,5 +26,7 @@ public class Llibre {
           System.out.println(this.author);
           
       }
-      
+      public boolean mateixAutor(Llibre llibre){
+        return this.author.equals(llibre.author);
+      }
 }
